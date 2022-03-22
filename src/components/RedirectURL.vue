794ea999895d08068c070c96ad283637fa8cc49a
Kyungsu Park <kyungsu77777@naver.com>
2022-03-23 00:56:31 +0900

    /api/url/redirect 로 REST URI 구조 변경

--- src/components/RedirectURL.vue
+++ src/components/RedirectURL.vue
@@ -1,12 +1,12 @@
 <template>
- 
+
 </template>
 
 <script>
 export default {
   name: 'RedirectURL',
   mounted(){
-    window.location.href = this.$axios.defaults.baseURL + "/api" + this.$route.fullPath;
+    window.location.href = this.$axios.defaults.baseURL + "/api/url/redirect" + this.$route.fullPath;
   },
 };
 </script>
